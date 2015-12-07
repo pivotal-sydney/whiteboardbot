9b0f6e9e259a631818f50069cbf26fb7e6b1c63e
Andrew Leung and Dariusz Lorenc <pair+aleung+dlorenc@pivotal.io>
2015-12-07 12:09:51 +1100

    While faces entry started, setting date will now set the date of the face entry.  Fixes issue where keywords were not parsed correctly. [#109420116]

--- app/slack_client.go
+++ app/slack_client.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"fmt"
 	. "github.com/xtreme-andleung/whiteboardbot/entry"
+	"time"
 )
 
 var face Face
@@ -26,9 +27,17 @@ func ParseMessageEvent(client SlackClient, clock Clock, ev *slack.MessageEvent) 
 		if strings.HasPrefix(message, "faces") {
 			face = NewFace(clock)
 			message = face.String()
-		} else if strings.HasPrefix(message, "name") {
+		} else if strings.HasPrefix(message, "name ") {
 			face.Name = message[5:]
 			message = face.String()
+		} else if strings.HasPrefix(message, "date ") {
+			parsedDate, err := time.Parse("2006-01-02", message[5:])
+			if err != nil {
+				message = face.String() + "\nDate not set, use YYYY-MM-DD as date format"
+			} else {
+				face.Time = parsedDate
+				message = face.String()
+			}
 		} else {
 			message = strings.Join([]string{user.Name, "no you", message}, " ")
 		}
--- spec/whiteboardbot_test.go
+++ spec/whiteboardbot_test.go
@@ -13,7 +13,8 @@ var _ = Describe("Whiteboardbot", func() {
 		helloWorldEvent slack.MessageEvent
 		newFaceEvent slack.MessageEvent
 		randomEvent slack.MessageEvent
-		addNameEvent slack.MessageEvent
+		setNameEvent slack.MessageEvent
+		setDateEvent slack.MessageEvent
 		client spec.MockSlackClient
 		clock spec.MockClock
 	)
@@ -30,8 +31,11 @@ var _ = Describe("Whiteboardbot", func() {
 		randomEvent.Text = "wbsome other text"
 		clock = spec.MockClock{}
 
-		addNameEvent = slack.MessageEvent{}
-		addNameEvent.Text = "wb name Dariusz Lorenc"
+		setNameEvent = slack.MessageEvent{}
+		setNameEvent.Text = "wb name Dariusz Lorenc"
+
+		setDateEvent = slack.MessageEvent{}
+		setDateEvent.Text = "wb date 2015-12-01"
 	})
 
 	Context("when receiving a MessageEvent", func() {
@@ -59,15 +63,47 @@ var _ = Describe("Whiteboardbot", func() {
 			})
 		})
 
-		Context("adding a name detail", func() {
+		Context("setting a name detail", func() {
 			Describe("with a new face entry started", func() {
 				BeforeEach(func() {
 					ParseMessageEvent(&client, clock, &newFaceEvent)
 				})
-				Describe("setting the name", func() {
+				Describe("with correct keyword", func() {
 					It("should set the name of the entry and respond with face string", func() {
-						_, Text := ParseMessageEvent(&client, clock, &addNameEvent)
+						_, Text := ParseMessageEvent(&client, clock, &setNameEvent)
 						Expect(Text).To(Equal("faces\n  *name: Dariusz Lorenc\n  date: 2015-01-02"))
+					})
+				})
+				Describe("with incorrect keyword", func() {
+					It("should respond with default", func() {
+						setNameEvent.Text = "wb nameSomethingWrong"
+						_, Text := ParseMessageEvent(&client, clock, &setNameEvent)
+						Expect(Text).To(Equal("aleung no you nameSomethingWrong"))
+					})
+				})
+			})
+		})
+		Context("setting a date detail", func() {
+			Describe("with a new face entry started", func() {
+				BeforeEach(func() {
+					ParseMessageEvent(&client, clock, &newFaceEvent)
+				})
+				Describe("with correct keyword", func() {
+					It("should set the date of the entry and respond with face string", func() {
+						_, Text := ParseMessageEvent(&client, clock, &setDateEvent)
+						Expect(Text).To(Equal("faces\n  *name: \n  date: 2015-12-01"))
+					})
+					It("should not set invalid date and respond with help message", func() {
+						setDateEvent.Text = "wb date 12/01/2015"
+						_, Text := ParseMessageEvent(&client, clock, &setDateEvent)
+						Expect(Text).To(Equal("faces\n  *name: \n  date: 2015-01-02\nDate not set, use YYYY-MM-DD as date format"))
+					})
+				})
+				Describe("with incorrect keyword", func() {
+					It("should respond with default", func() {
+						setDateEvent.Text = "wb date2015-12-01"
+						_, Text := ParseMessageEvent(&client, clock, &setDateEvent)
+						Expect(Text).To(Equal("aleung no you date2015-12-01"))
 					})
 				})
 			})
