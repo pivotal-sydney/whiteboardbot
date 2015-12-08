ef7096350d151f30f34318306dc5e8e0066a112c
Michael Freedman <pair+michael@pivotallabs.com>
2015-12-08 14:59:29 +1100

    Adds a test for update face request [#109420214]

--- spec/whiteboard_request_test.go
+++ spec/whiteboard_request_test.go
@@ -18,6 +18,7 @@ var _ = Describe("Whiteboard Request", func() {
 	BeforeEach(func() {
 		face = entry.NewFace(spec.MockClock{}, "aleung")
 		face.Name = "Dariusz"
+		face.Id = "123"
 		os.Setenv("WB_AUTH_TOKEN", "token")
 	})
 
@@ -30,6 +31,25 @@ var _ = Describe("Whiteboard Request", func() {
 			Expect(request.Token).To(Equal("token"))
 			Expect(request.Commit).To(Equal("Create New Face"))
 			Expect(request.Id).To(Equal(""))
+			Expect(request.Item.StandupId).To(Equal(1))
+			Expect(request.Item.Title).To(Equal("Dariusz"))
+			Expect(request.Item.Date).To(Equal("2015-01-02"))
+			Expect(request.Item.PostId).To(Equal(""))
+			Expect(request.Item.Public).To(Equal("false"))
+			Expect(request.Item.Kind).To(Equal("New face"))
+			Expect(request.Item.Description).To(Equal(""))
+			Expect(request.Item.Author).To(Equal("aleung"))
+		})
+	})
+	Describe("when creating a NewUpdateFaceRequest", func() {
+		It("should create request", func() {
+			var request FaceRequest
+			request = NewUpdateFaceRequest(face)
+			Expect(request.Utf8).To(Equal(""))
+			Expect(request.Method).To(Equal("patch"))
+			Expect(request.Token).To(Equal("token"))
+			Expect(request.Commit).To(Equal("Update New Face"))
+			Expect(request.Id).To(Equal(face.Id))
 			Expect(request.Item.StandupId).To(Equal(1))
 			Expect(request.Item.Title).To(Equal("Dariusz"))
 			Expect(request.Item.Date).To(Equal("2015-01-02"))
