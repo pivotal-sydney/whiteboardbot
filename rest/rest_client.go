744aaebc8d1e442a7b51cc169aecf02fb4fcbe3d
Andrew Leung and Dariusz Lorenc <pair+aleung+dlorenc@pivotal.io>
2015-12-14 18:15:01 +1100

    Fixes typo in env variable [#109851342]

--- rest/rest_client.go
+++ rest/rest_client.go
@@ -21,7 +21,7 @@ func (RealRestClient) Post(request model.WhiteboardRequest) (itemId string, ok b
 	fmt.Printf("Posting entry to whiteboard:\n%v\n", string(json))
 	http.DefaultClient.CheckRedirect = noRedirect
 	url := os.Getenv("WB_HOST_URL")
-	standupId := os.Getenv("WB_STANDUP_IP")
+	standupId := os.Getenv("WB_STANDUP_ID")
 	if len(request.Id) > 0 {
 		url += "/items/" + request.Id
 	} else {
