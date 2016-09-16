c43e75286a84199939d8b9a102fd40f9d883f93f
Benjamin Tan Wei Hao <btan@pivotal.io>
2016-09-16 10:48:59 +0800

    Testing around registering standups

--- spec/quiet_whiteboard_test.go
+++ spec/quiet_whiteboard_test.go
@@ -1,20 +1,27 @@
 package spec
 
 import (
+	"encoding/json"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/pivotal-sydney/whiteboardbot/app"
+	. "github.com/pivotal-sydney/whiteboardbot/model"
 )
 
 var _ = Describe("QuietWhiteboard", func() {
 
 	var (
-		whiteboard QuietWhiteboardApp
+		whiteboard    QuietWhiteboardApp
+		store         MockStore
+		sydneyStandup Standup
 	)
 
 	BeforeEach(func() {
+		sydneyStandup = Standup{Id: 133, TimeZone: "Australia/Sydney", Title: "Sydney"}
+
 		restClient := MockRestClient{}
-		store := MockStore{}
+		store = MockStore{}
+
 		whiteboard = NewQuietWhiteboard(&restClient, &store)
 	})
 
@@ -23,6 +30,36 @@ var _ = Describe("QuietWhiteboard", func() {
 			It("should return the usage text", func() {
 				expected := Response{Text: USAGE}
 				Expect(whiteboard.HandleInput("?")).To(Equal(expected))
+			})
+		})
+
+		Context("register", func() {
+			It("stores the standup in the store", func() {
+				expectedStandupJson, _ := json.Marshal(sydneyStandup)
+				expectedStandupString := string(expectedStandupJson)
+
+				whiteboard.HandleInput("register 133")
+
+				standupString, standupPresent := store.Get("133")
+				Expect(standupPresent).To(Equal(true))
+				Expect(standupString).To(Equal(expectedStandupString))
+			})
+
+			It("returns a message with the registered standup", func() {
+				expected := Response{Text: "Standup Sydney has been registered! You can now start creating Whiteboard entries!"}
+				Expect(whiteboard.HandleInput("register 1234")).To(Equal(expected))
+			})
+
+			Context("when standup does not exist", func() {
+				It("returns a message that the standup isn't found", func() {
+					// expected := Response{Text: "Standup not found!"}
+					// Expect(whiteboard.HandleInput("register 123")).To(Equal(expected))
+				})
+
+				It("does not store anything in the store", func() {
+					// whiteboard.HandleInput("register 123")
+					// Expect(len(store.StoreMap)).To(Equal(0))
+				})
 			})
 		})
 	})
